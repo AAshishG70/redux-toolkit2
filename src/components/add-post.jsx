d67e6e1453725563b4614c069e323e8beb1ff18c
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2023-02-25 21:37:53 -0700

    add functionality to the add post

--- src/components/add-post.jsx
+++ src/components/add-post.jsx
@@ -1,16 +1,52 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 const AddPost = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState();
+  const [userId, setUserId] = useState();
+  const [content, setContent] = useState();
+
+  const submitHandler = () => {
+    if (title && content && userId) {
+      setTitle("");
+      setUserId("");
+      setContent("");
+    }
+  };
+
   return (
     <section>
-      <h2>Add a Post</h2>
-      <label htmlFor="postTitle">Title: </label>
-      <input type="text" id="postTitle" value="postTitle" placeholder="Title" />
-      <br />
-      <label htmlFor="postAutor">Author:</label>
-      <select htmlFor="postAuthor"> </select>
-      <option value=""></option>
-      <label htmlFor="postContent">Content: </label>
-      <textarea id="postContent" value="postContent" />
-      <button type="button">Save a Post</button>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title: </label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select
+          id="postAuthor"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        >
+          <option value=""></option>
+        </select>
+        <label htmlFor="postContent">Content: </label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="button" onClick={submitHandler}>
+          Save Post
+        </button>
+      </form>
     </section>
   );
 };
